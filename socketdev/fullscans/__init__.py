c5d37a1cf0bdc168db8a698dbf832b42f2ae59a9
Orlando Barrera II <orlando@socket.dev>
2024-06-14 10:15:11 -0500

    Added the Fullscans API functions GET, POST

--- socketdev/fullscans/__init__.py
+++ socketdev/fullscans/__init__.py
@@ -1,5 +1,5 @@
 import socketdev
-
+from socketdev.tools import load_files
 
 class Fullscans:
     @staticmethod
@@ -19,23 +19,46 @@ class Fullscans:
             result = {}
         return result
     
-    def post(org_slug: str) -> dict:
-        path = "orgs/"+org_slug+"/full-scans"
-        headers = {
-            "accept": "application/json",
-            "content-type": "multipart/form-data"
-        }
-        payload = None
+    @staticmethod
+    def post(
+            files: list, 
+            params: dict
+            ) -> dict:
+        
+        loaded_files = []
+        loaded_files = load_files(files, loaded_files)
+
+        params_arg = "?"
+        params_arg  = params_arg +"repo="+params["repo"]
+        if params["branch"]:
+            params_arg  = params_arg +"&branch="+params["branch"]
+        if params["commit_message"]:
+            params_arg  = params_arg +"&commit_message="+params["commit_message"]
+        if params["commit_hash"]:
+            params_arg  = params_arg +"&commit_hash="+params["commit_hash"]
+        if params["pull_request"]:
+            params_arg  = params_arg +"&pull_request="+params["pull_request"]
+        if params["committers"]:
+            params_arg  = params_arg +"&committers="+params["committers"]
+        if params["make_default_branch"]:
+            params_arg  = params_arg +"&make_default_branch="+params["make_default_branch"]
+        if params["set_as_pending_head"]:
+            params_arg  = params_arg +"&set_as_pending_head="+params["set_as_pending_head"]
+        if params["tmp"]:
+            params_arg  = params_arg +"&tmp="+params["tmp"]
+
+        path = "orgs/"+params["org_slug"]+"/full-scans"+params_arg
 
         response = socketdev.do_request(
             path=path,
             method="POST",
-            headers=headers,
-            payload=payload
+            files=loaded_files
         )
-        if response.status_code == 200:
-            result = response.text
+
+        if response.status_code == 201:
+            result = response.json()
         else:
-            print(f"Error posting {org_slug} to the Fullscans API")
+            print(f"Error posting {files} to the Fullscans API")
             print(response.text)
+            result = response.text
         return result
