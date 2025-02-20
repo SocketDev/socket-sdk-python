36212a19143683225b088fe751813163b7224c27
Douglas Coburn <douglas@dactbc.com>
2025-02-19 19:55:05 -0800

    Payload needed to be str not dict

--- socketdev/repos/__init__.py
+++ socketdev/repos/__init__.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import List, Optional
 from dataclasses import dataclass, asdict
@@ -131,7 +132,8 @@ class Repos:
             return {}
             
         path = "orgs/" + org_slug + "/repos"
-        response = self.api.do_request(path=path, method="POST", payload=params)
+        payload = json.dumps(params)
+        response = self.api.do_request(path=path, method="POST", payload=payload)
         
         if response.status_code == 200:
             result = response.json()
@@ -150,7 +152,8 @@ class Repos:
             return {}
             
         path = f"orgs/{org_slug}/repos/{repo_name}"
-        response = self.api.do_request(path=path, method="POST", payload=params)
+        payload = json.dumps(params)
+        response = self.api.do_request(path=path, method="POST", payload=payload)
         
         if response.status_code == 200:
             result = response.json()
